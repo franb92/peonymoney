eb3a5cb03cb71150a24db167df37a558afaf2443
franb92 <b.franettovich@hotmail.com>
2021-09-11 10:30:50 +1000

    Updatedserializersandviewfile

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Project
+from .serializers import ProjectSerializer
+
+#for /projects
+class ProjectList(APIView):
+
+    #for GET /projects
+    def get(self, request):
+        #get all the projects
+        projects = Project.objects.all()
+        #serialise all the projects
+        serializer = ProjectSerializer(projects, many=True)
+        # send all the serialized projects back in response body
+        return Response(serializer.dat
