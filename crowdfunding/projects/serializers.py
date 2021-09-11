607763140672d001dac2590888ff968dcadfb9c7
franb92 <b.franettovich@hotmail.com>
2021-09-11 12:51:38 +1000

    responses

--- crowdfunding/projects/serializers.py
+++ crowdfunding/projects/serializers.py
@@ -1,5 +1,16 @@
 from rest_framework import serializers
-from .models import Project
+from .models import Project, Pledge
+
+class PledgeSerializer(serializers.Serializer):
+    id = serializers.ReadOnlyField()
+    amount = serializers.IntegerField()
+    comment = serializers.CharField(max_length=200)
+    anonymous = serializers.BooleanField()
+    supporter = serializers.CharField(max_length=200)
+    project_id = serializers.IntegerField()
+    
+    def create(self, validated_data):
+        return Pledge.objects.create(**validated_data)
 
 class ProjectSerializer(serializers.Serializer):
     id = serializers.ReadOnlyField()
--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -4,8 +4,30 @@ from django.shortcuts import render
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .models import Project
-from .serializers import ProjectSerializer
+from .models import Project, Pledge
+from .serializers import ProjectSerializer, PledgeSerializer
+
+class 
+PledgeList(APIView):
+    def get(self, request):
+        pledges = Pledge.objects.all()
+        serializer = PledgeSerializer(pledges, many=True)
+        return Response(serializer.data)
+        
+    def post(self, request):
+        serializer = PledgeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+            
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
 #for /projects
 class ProjectList(APIView):
