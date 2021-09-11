735a5b402c1ae780c17ea7867118578ed1fdce52
franb92 <b.franettovich@hotmail.com>
2021-09-11 12:05:57 +1000

    updatingURLSandviews

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Project
@@ -23,4 +24,13 @@ class ProjectList(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
-    
+
+class ProjectDetail(APIView):
+    def get_object(self, pk):
+        return Project.objects.get(pk=pk)
+        
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
+
